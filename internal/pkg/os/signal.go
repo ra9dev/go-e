2b6ede6d96f8f73e6ee1e783b35a593ae7701112
Sherkhan Kubaidullov <sherkhan@boostylabs.com>
2022-02-12 20:57:51 +0300

    comments

--- internal/pkg/os/signal.go
+++ internal/pkg/os/signal.go
@@ -8,6 +8,7 @@ import (
 	"syscall"
 )
 
+// CatchTermination listens for os signals to cancel global context
 func CatchTermination(cancel context.CancelFunc) {
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
